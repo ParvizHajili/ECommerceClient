37fdcdd53879f3c398c1a17f7beb92dbf829e052
ParvizHacili <parviz.hajili@gmail.com>
2022-04-25 01:29:53 +0400

    Validation Filter with Fluentvalidation

--- src/app/admin/components/products/create/create.component.ts
+++ src/app/admin/components/products/create/create.component.ts
@@ -2,24 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from '../../../../base/base.component';
 import { Create_Product } from '../../../../contracts/create_product';
-import { AlertifyService, MessageType, Position } from '../../../../services/admin/alertify.service';
+import {
+  AlertifyService,
+  MessageType,
+  Position,
+} from '../../../../services/admin/alertify.service';
 import { ProductService } from '../../../../services/common/models/product.service';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss']
+  styleUrls: ['./create.component.scss'],
 })
 export class CreateComponent extends BaseComponent implements OnInit {
-
-  constructor(spinner: NgxSpinnerService, private productService: ProductService, private alertify: AlertifyService) {
-    super(spinner)
+  constructor(
+    spinner: NgxSpinnerService,
+    private productService: ProductService,
+    private alertify: AlertifyService
+  ) {
+    super(spinner);
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  create(name: HTMLInputElement, stock: HTMLInputElement, price: HTMLInputElement) {
+  create(
+    name: HTMLInputElement,
+    stock: HTMLInputElement,
+    price: HTMLInputElement
+  ) {
     this.showSpinner(SpinnerType.BallAtom);
 
     const create_product: Create_Product = new Create_Product();
@@ -27,13 +37,43 @@ export class CreateComponent extends BaseComponent implements OnInit {
     create_product.stock = parseInt(stock.value);
     create_product.price = parseFloat(price.value);
 
-    this.productService.create(create_product, () => {
-      this.hideSpinner(SpinnerType.BallAtom);
-      this.alertify.message("Məhsul Uöurla Əlavə Edildi", {
+    if (!name.value) {
+      this.alertify.message('Məhsul Adını Əlavə Edin', {
         dismissOthers: true,
-        messageType: MessageType.Success,
-        position: Position.TopRight
-      })
-    });
+        messageType: MessageType.Error,
+        position: Position.TopRight,
+      });
+
+      return;
+    }
+
+    if (parseInt(stock.value) < 0) {
+      this.alertify.message('Məhsul Sayı 0 dan az ola bilməz!!', {
+        dismissOthers: true,
+        messageType: MessageType.Error,
+        position: Position.TopRight,
+      });
+
+      return;
+    }
+
+    this.productService.create(
+      create_product,
+      () => {
+        this.hideSpinner(SpinnerType.BallAtom);
+        this.alertify.message('Məhsul Uğurla Əlavə Edildi', {
+          dismissOthers: true,
+          messageType: MessageType.Success,
+          position: Position.TopRight,
+        });
+      },
+      (errorMessage) => {
+        this.alertify.message(errorMessage, {
+          dismissOthers: true,
+          messageType: MessageType.Error,
+          position: Position.TopRight,
+        });
+      }
+    );
   }
 }
