6540a0446f64d1a9a27fbd19e49f7ae430dc57d5
ParvizHacili <parviz.hajili@gmail.com>
2022-05-13 14:19:15 +0400

    Delete Operation with Delete Directive

--- src/app/admin/components/products/list/list.component.ts
+++ src/app/admin/components/products/list/list.component.ts
@@ -6,6 +6,9 @@ import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 import { List_Product } from 'src/app/contracts/list_product';
 import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
 import { ProductService } from 'src/app/services/common/models/product.service';
+
+declare var $: any;
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
@@ -17,7 +20,7 @@ export class ListComponent extends BaseComponent implements OnInit  {
     super(spinner)
   }
 
-  displayedColumns: string[] = ['name', 'stock', 'price', 'createdDate','updatedDate'];
+  displayedColumns: string[] = ['name', 'stock', 'price', 'createdDate','updatedDate','edit','delete'];
   dataSource: MatTableDataSource<List_Product> = null;
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
@@ -33,6 +36,11 @@ export class ListComponent extends BaseComponent implements OnInit  {
     this.dataSource = new MatTableDataSource<List_Product>(allProducts.products);
     this.paginator.length = allProducts.totalCount;
   }
+
+  // delete(id,event){
+  //   const img: HTMLImageElement = event.srcElement;
+  //   $(img.parentElement.parentElement).fadeOut(1000)
+  // }
 
   async pageChanged(){
     await this.getProducts();
--- src/app/base/base.component.ts
+++ src/app/base/base.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 
 export class BaseComponent {
-  constructor(private spinner: NgxSpinnerService) {}
+  constructor(private spinner: NgxSpinnerService) { }
 
   showSpinner(spinnerNameType: SpinnerType) {
-    this.spinner.show(spinnerNameType);
+    this.spinner.show(SpinnerType.BallTrianglePath);
 
-    setTimeout(() => this.hideSpinner(spinnerNameType), 1000);
+    //setTimeout(() => this.hideSpinner(spinnerNameType), 1000);
   }
+
   hideSpinner(spinnerNameType: SpinnerType) {
     this.spinner.hide(spinnerNameType);
   }
