83c684e967c91b233eeabad3e7732ce0c52f0130
ParvizHacili <parviz.hajili@gmail.com>
2022-05-28 22:07:46 +0400

    Revert "Listing and Pagination of Products"
    This reverts commit 9b4a532a4268e81db3b5f8d93d8fe11676a5a581.

--- src/app/admin/components/products/products.module.ts
+++ src/app/admin/components/products/products.module.ts
@@ -8,20 +8,22 @@ import { ListComponent } from './list/list.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
+
+
 
 @NgModule({
-  declarations: [ProductsComponent, CreateComponent, ListComponent],
+  declarations: [
+    ProductsComponent,
+    CreateComponent,
+    ListComponent
+  ],
   imports: [
     CommonModule,
-    RouterModule.forChild([{ path: '', component: ProductsComponent }]),
-    MatSidenavModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatTableModule,
-    MatPaginatorModule,
-  ],
+    RouterModule.forChild([
+      { path: "", component: ProductsComponent }
+    ]),
+    MatSidenavModule, MatFormFieldModule,
+    MatInputModule, MatButtonModule
+  ]
 })
-export class ProductsModule {}
+export class ProductsModule { }
--- src/app/services/common/models/product.service.ts
+++ src/app/services/common/models/product.service.ts
@@ -1,6 +1,5 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { List_Product } from 'src/app/contracts/list_product';
 import { Create_Product } from '../../../contracts/create_product';
 import { HttpClientService } from '../http-client.service';
 
@@ -10,11 +9,7 @@ import { HttpClientService } from '../http-client.service';
 export class ProductService {
   constructor(private httpClientService: HttpClientService) {}
 
-  create(
-    product: Create_Product,
-    successCallBack?: () => void,
-    errorCallBack?: (errorMessage: string) => void
-  ) {
+  create(product: Create_Product, successCallBack?: any, errorCallBack?: any) {
     this.httpClientService
       .post(
         {
@@ -38,27 +33,5 @@ export class ProductService {
           errorCallBack(message);
         }
       );
-  }
-
-  async read(
-    page: number = 0,
-    size: number = 5,
-    successCallBack?: () => void,
-    errorCallBack?: (errorMessage: string) => void
-  ): Promise<{totalCount: number; products: List_Product[]}> {
-    const promiseData: Promise<{totalCount: number; products: List_Product[]}> = this.httpClientService
-      .get<{totalCount: number; products: List_Product[]}>({
-        controller: 'products',
-        queryString: `page=${page}&size=${size}`
-      })
-      .toPromise();
-
-    promiseData
-      .then((d) => successCallBack())
-      .catch((errorResponse: HttpErrorResponse) =>
-        errorCallBack(errorResponse.message)
-      );
-
-    return await promiseData;
   }
 }
